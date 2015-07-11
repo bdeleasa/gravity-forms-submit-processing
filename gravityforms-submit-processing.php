2d20241aacdea3bd9129cd1633ed604e332ef8bf
Brianna Deleasa <bdeleasa@gmail.com>
2015-07-11 09:15:51 -0400

    Renaming the plugin's files to match the new folder and repo name.

--- gravityforms-submit-processing.php
+++ gravityforms-submit-processing.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Gravity Forms Submit Processing
  * Plugin URI:        http://chooserethink.com
  * Description:       Disables the submit button upon submitting a Gravity Forms form.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            Brianna Deleasa @ re:think
  * Author URI:        http://chooserethink.com
  * License:           GPL-2.0+
@@ -15,7 +15,7 @@
 add_action( 'wp_enqueue_scripts', 'gravity_forms_submit_processing_scripts' );
 function gravity_forms_submit_processing_scripts() {
 
-    wp_enqueue_style( 'gfsp-css', plugins_url('css/gravity-forms-submit-processing.css', __FILE__), false, null );
-    wp_enqueue_script( 'gfsp-js', plugins_url('js/gravity-forms-submit-processing.js', __FILE__) , array('jquery'), null, true );
+    wp_enqueue_style( 'gfsp-css', plugins_url('css/gravityforms-submit-processing.css', __FILE__), false, null );
+    wp_enqueue_script( 'gfsp-js', plugins_url('js/gravityforms-submit-processing.js', __FILE__) , array('jquery'), null, true );
 
 }
