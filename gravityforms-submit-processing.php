1146e2b532c3b705e4c40db22f4bd91743ac74ed
Brianna Deleasa <bdeleasa@gmail.com>
2015-07-12 11:11:46 -0400

    Updating the plugin information and adding some comments.

--- gravityforms-submit-processing.php
+++ gravityforms-submit-processing.php
@@ -1,15 +1,31 @@
 <?php
 /**
  * Plugin Name:       Gravity Forms Submit Processing
- * Plugin URI:        http://chooserethink.com
- * Description:       Disables the submit button upon submitting a Gravity Forms form.
+ * Plugin URI:        https://github.com/bdeleasa/gravityforms-submit-processing
+ * Description:       Disables the submit button upon submitting a Gravity Forms form to help prevent multiple submissions.
  * Version:           2.0.0
- * Author:            Brianna Deleasa @ re:think
- * Author URI:        http://chooserethink.com
+ * Author:            Brianna Deleasa
+ * Author URI:        http://briannadeleasa.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       gfsp
  * Domain Path:       /languages
+ *
+ * Gravity Forms Submit Processing
+ * Copyright (C) 2014 Brianna Deleasa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
